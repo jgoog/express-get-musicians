619346450dad59b8651c717b5a8e352d04947bf7
Johnny <aguguaj@hotmail.com>
2023-07-03 15:22:38 -0500

    All test pass exercise complete

--- index.test.js
+++ index.test.js
@@ -5,18 +5,25 @@ execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician , Band} = require('./models/index')
 const app = require('./src/app');
-const seedMusician = require("./seedData");
+const {seedMusician, seedBand }= require("./seedData");
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 
 
 describe('./musicians endpoint', () => {
+
+    beforeAll(async () => {
+        await db.sync({ force: true });
+        await Musician.bulkCreate(seedMusician);
+        await Band.bulkCreate(seedBand);
+      });
     // Write your tests here
-    
-    
-
-
-
-
-    
+    it('should return musicians', async () =>{
+        const response = await request(app).get('/musicians');
+        expect(response.statusCode).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData).toBeInstanceOf(Array);
+        expect(responseData.length).toBeGreaterThan(0);
+    })
 })
